3f2c8fa9e4256b1063cc82b4b9d66cd51e724a3e
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-04-22 17:57:23 -0300

    Configurando Autenticação e Autorização com JWT Bearer, iniciando service de login.

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace blogV2
+{
+    public static class Configuration
+    {
+        public static string JwtKey { get; set; } = "MDI4YTcxNDAtZTRjOS00NDhjLWE5NGEtOGZhMjJlMjYxODI3=";
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using BlogV2.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace blogV2.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(new Claim []
+                {
+                    new (ClaimTypes.Name, value: "saulocosta"),
+                    new (ClaimTypes.Role, value: "user"),
+                    new (ClaimTypes.Role, value: "admin"),
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), 
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
