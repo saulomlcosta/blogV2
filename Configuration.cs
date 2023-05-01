0e7c1cdb221b7bf762d1363eea13e154c8c33314
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-05-01 16:45:28 -0300

    Posibilitando conexão via API_KEY.

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,8 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "MDI4YTcxNDAtZTRjOS00NDhjLWE5NGEtOGZhMjJlMjYxODI3=";
+        public static string JwtKey = "MDI4YTcxNDAtZTRjOS00NDhjLWE5NGEtOGZhMjJlMjYxODI3=";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "course_api_P2VWrjndi7Ujd/ihQKtGuw==";
     }
 }
