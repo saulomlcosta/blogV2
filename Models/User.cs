c8379aab24c7ca96592320372a40a0e9499ea6f6
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-05-10 21:31:00 -0300

    Evitando renderização de uma prop específica, no caso Password.

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace BlogV2.Models;
 
 public class User
@@ -5,6 +7,7 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    [JsonIgnore]
     public string PasswordHash { get; set; }
     public string Image { get; set; }
     public string Slug { get; set; }
