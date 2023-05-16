ffed3d4bcc0d15a0f8f49f7e3f0314ffcf7c2fdd
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-05-16 10:05:48 -0300

    Implementando dentro do endpoint de Posts, paginação, ordenação e retornando para o front de forma amigável.

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,6 @@
 using BlogV2.Data;
+using BlogV2.Models;
+using BlogV2.ViewModels;
 using BlogV2.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,25 +12,45 @@ namespace BlogV2.Controllers
     {
         [HttpGet("v1/posts")]
         public async Task<IActionResult> GetAsync(
-            [FromServices] BlogDataContext context
+            [FromServices] BlogDataContext context,
+            [FromQuery] int Page = 0,
+            [FromQuery] int PageSize = 25
         )
         {
-            var posts = await context
-            .Posts
-            .AsNoTracking()
-            .Include(x => x.Category)
-            .Include(x => x.Author)
-            // .Select(x => new ListPostsViewModel
-            // {
-            //     Id = x.Id,
-            //     Title = x.Title,
-            //     Slug = x.Slug,
-            //     LastUpdateDate = x.LastUpdateDate,
-            //     Category = x.Category.Name,
-            //     Author = $"{x.Author.Name} ({x.Author.Email})"
-            // })
-            .ToListAsync();
-            return Ok(posts);
+            try
+            {
+                var count = await context.Posts.CountAsync();
+                var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(Page)
+                .Take(PageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    total = count,
+                    page = Page,
+                    pageSize = PageSize,
+                    posts
+                }));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<Post>("05X04 - Falha interna no servidor"));
+            }
         }
     }
 }
