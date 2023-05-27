c5e16d12c1ddb7e5802bb14262ce784c24437481
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-05-27 14:14:50 -0300

    Finalizando limpeza de warnings.

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -11,9 +11,9 @@ public class BlogDataContext : DbContext
     {
     }
 
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Post> Posts { get; set; }
-    public DbSet<User> Users { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Post>? Posts { get; set; }
+    public DbSet<User>? Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
