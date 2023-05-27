9e979e923cec4e8898c332262e5f17ed1c126bea
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-05-27 11:42:56 -0300

    Reorganizando Serviços, Configurações, Authenticação etc em uma classe separada.

---
+++ Extensions/AppExtension.cs
@@ -0,0 +1,77 @@
+using System.IO.Compression;
+using System.Text;
+using System.Text.Json.Serialization;
+using BlogV2.Data;
+using BlogV2.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogV2.Extensions;
+
+public static class AppExtension
+{
+    public static void LoadConfiguration(this WebApplicationBuilder builder)
+    {
+        Configuration.JwtKey = builder.Configuration.GetValue<string>("JwtKey");
+        Configuration.ApiKeyName = builder.Configuration.GetValue<string>("ApiKeyName");
+        Configuration.ApiKeyValue = builder.Configuration.GetValue<string>("ApiKeyValue");
+
+        var smtp = new Configuration.SmtpConfiguration();
+        builder.Configuration.GetSection("Smtp").Bind(smtp);
+        Configuration.Smtp = smtp;
+    }
+
+    public static void ConfigureAuthentication(this WebApplicationBuilder builder)
+    {
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    }
+
+    public static void ConfigureMvc(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddResponseCompression(options =>
+        {
+            //options.Providers.Add<BrotliCompressionProvider>();
+            options.Providers.Add<GzipCompressionProvider>();
+            //options.Providers.Add<CustomCompressionProvider>();
+        });
+        builder.Services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+        builder.Services.AddMemoryCache();
+        builder.Services
+            .AddControllers()
+            .ConfigureApiBehaviorOptions(
+                options => options.SuppressModelStateInvalidFilter = true)
+            .AddJsonOptions(x =>
+            {
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                x.JsonSerializerOptions.DefaultIgnoreCondition
+                    = JsonIgnoreCondition.WhenWritingDefault; // Caso o objeto venha nulo, não renderizar.
+            });
+    }
+
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<BlogDataContext>(
+            options =>
+                options.UseSqlServer(connectionString));
+
+        builder.Services.AddTransient<TokenService>();
+        builder.Services.AddTransient<EmailService>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,16 @@
-using BlogV2;
-using BlogV2.Services;
-using BlogV2.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.ResponseCompression;
-using System.IO.Compression;
-using Microsoft.EntityFrameworkCore;
+using BlogV2.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-ConfigureAuthentication(builder);
-ConfigureMvc(builder);
-ConfigureServices(builder);
+
+builder.LoadConfiguration();
+builder.ConfigureAuthentication();
+builder.ConfigureMvc();
+builder.ConfigureServices();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-LoadConfiguration(app);
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
@@ -34,69 +26,3 @@ if (app.Environment.IsDevelopment())
 }
 
 app.Run();
-
-void LoadConfiguration(WebApplication app)
-{
-    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-    Configuration.ApiKeyValue = app.Configuration.GetValue<string>("ApiKeyValue");
-
-    var smtp = new Configuration.SmtpConfiguration();
-    app.Configuration.GetSection("Smtp").Bind(smtp);
-    Configuration.Smtp = smtp;
-}
-
-void ConfigureAuthentication(WebApplicationBuilder builder)
-{
-    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-    builder.Services.AddAuthentication(x =>
-    {
-        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(x =>
-    {
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-}
-
-void ConfigureMvc(WebApplicationBuilder builder)
-{
-    builder.Services.AddResponseCompression(options =>
-    {
-        //options.Providers.Add<BrotliCompressionProvider>();
-        options.Providers.Add<GzipCompressionProvider>();
-        //options.Providers.Add<CustomCompressionProvider>();
-    });
-    builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-    {
-        options.Level = CompressionLevel.Optimal;
-    });
-    builder.Services.AddMemoryCache();
-    builder.Services
-        .AddControllers()
-        .ConfigureApiBehaviorOptions(
-            options => options.SuppressModelStateInvalidFilter = true)
-        .AddJsonOptions(x =>
-        {
-            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-            x.JsonSerializerOptions.DefaultIgnoreCondition
-                = JsonIgnoreCondition.WhenWritingDefault; // Caso o objeto venha nulo, não renderizar.
-        });
-}
-
-void ConfigureServices(WebApplicationBuilder builder)
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<BlogDataContext>(
-        options =>
-            options.UseSqlServer(connectionString));
-
-    builder.Services.AddTransient<TokenService>();
-    builder.Services.AddTransient<EmailService>();
-}
